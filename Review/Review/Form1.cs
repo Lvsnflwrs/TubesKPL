9a3b55408f483495eaac5b945f33283927531dad
isabella <bellamanope@gmail.com>
2024-06-20 09:51:56 +0700

    Review

--- Review/Review/Form1.cs
+++ Review/Review/Form1.cs
@@ -16,8 +16,11 @@ namespace Review
 {
     public partial class Form1 : Form
     {
+        // Properti untuk menyimpan informasi produk
         private String namaProduk;
         private int hargaProduk;
+
+        //constructor untuk menginisialisasi form dengan data produk
         public Form1(String namaProduk, int hargaProduk)
         {
             InitializeComponent();
@@ -27,81 +30,24 @@ namespace Review
 
             label5.Text = namaProduk;
             label6.Text = hargaProduk.ToString();
-            
         }
 
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void pictureBox4_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void pictureBox6_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label3_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label4_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void pictureBox3_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label6_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label5_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-            
-        }
-
+        //Internal class untuk mendeteksi spam berdasarkan dari review.
         internal class SpamDetector
         {
+            // Enumerasi yang mewakili kemungkinan status spam pada deskripsi review.
             public enum State { Start, PossibleSpam, CertainSpam, NotSpam };
 
+            // Variabel untuk menyimpan status spam saat ini.
             private State currentState;
 
+            // Menginisialisasi status spam saat ini ke Start.
             public SpamDetector()
             {
                 currentState = State.Start;
             }
 
+            // Method untuk mendeteksi apakah review spam atau tidak.
             public bool DetectSpam(string review)
             {
                 foreach (string word in review.ToLower().Split(' '))
@@ -121,23 +67,25 @@ namespace Review
 
                         if (charCount[c] > 2)
                         {
-                            return true;
+                            return true; // Jika ada karakter yang muncul lebih dari 2 kali, dianggap sebagai spam
                         }
                     }
                 }
-
-                return false;
+                return false; // Tidak ditemukan karakteristik spam pada review.
             }
 
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            // Mendapatkan nilai dari TextBox untuk review dan rating
             string review = textBox2.Text;
             string rate = textBox1.Text;
 
+            // Membuat instance dari kelas SpamDetector untuk mendeteksi spam pada review
             SpamDetector detector = new SpamDetector();
 
+            // Memeriksa apakah review kosong
             if (string.IsNullOrEmpty(review))
             {
                 MessageBox.Show("Description cannot be empty");
@@ -148,18 +96,22 @@ namespace Review
             }
             else
             {
+                // Memeriksa apakah review terdeteksi sebagai spam
                 if (detector.DetectSpam(review))
                 {
                     MessageBox.Show("This review is detected as spam.");
-                    textBox2.Clear();
-                } 
+                    textBox2.Clear();// mengosongkan TextBox review
+                }
+                // Memeriksa apakah input rating valid (berupa angka)
                 else if (!Regex.IsMatch(rate, @"^\d+$")) 
                 { 
                     MessageBox.Show("The rating must be a number", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
                 else
                 {
+                    // Mengonversi input rating dari string menjadi integer
                     int rating = int.Parse(rate);
+                    // Memeriksa apakah rating berada dalam rentang yang valid (0-5)
                     if (rating < 0 || rating > 5)
                     {
                         MessageBox.Show("Give a rating out of 5");
@@ -167,11 +119,17 @@ namespace Review
                     else
                     {
                         MessageBox.Show("rating berhasil dibuat!");
-
+                        // Menyembunyikan form ini
                         this.Hide();
+
+                        // Membuat instance dari Form2 dengan data produk
                         Form2 r = new Form2(namaProduk, hargaProduk);
+
+                        // Mengatur nilai TextBox pada Form2 dengan input pengguna
                         r.SetTextBox1(rate);
                         r.SetTextBox2(review);
+
+                        // Menampilkan Form2
                         r.Show();
                       }
                 }
@@ -179,6 +137,54 @@ namespace Review
         }
 
         private void pictureBox4_Click_1(object sender, EventArgs e)
+        {
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
         {
 
         }
--- Review/Review/Form2.cs
+++ Review/Review/Form2.cs
@@ -12,53 +12,59 @@ namespace Review
 {
     public partial class Form2 : Form
     {
-        private String namaProduk;
-        private int hargaProduk;
+        // Properti untuk menyimpan informasi produk
+        private String _namaProduk;
+        private int _hargaProduk;
 
+        //constructor untuk menginisialisasi form dengan data produk
         public Form2(String namaProduk, int hargaProduk)
         {
             InitializeComponent();
 
-            this.namaProduk = namaProduk;
-            this.hargaProduk = hargaProduk;
+            this._namaProduk = namaProduk;
+            this._hargaProduk = hargaProduk;
 
             label2.Text = namaProduk;
+        }
+
+        // Metode untuk mengatur nilai TextBox1 dengan nilai rate yang diberikan.
+        public void SetTextBox1(string rate)
+        {
+            textBox1.Text = rate;
+        }
+
+        //Metode untuk mengatur nilai TextBox2 dengan nilai review yang diberikan.
+        public void SetTextBox2(string review)
+        {
+            textBox2.Text = review;
+        }
+
+        // Event handler untuk click button1 kembali ke halaman sebelumnya.
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //backbutton
         }
 
         private void pictureBox2_Click(object sender, EventArgs e)
         {
 
         }
-
         private void label2_Click(object sender, EventArgs e)
         {
 
         }
-
         private void label3_Click(object sender, EventArgs e)
         {
 
         }
-
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
         }
-
         private void textBox2_TextChanged(object sender, EventArgs e)
         {
 
 
-        }
-
-        public void SetTextBox1(string rate)
-        {
-            textBox1.Text = rate;
-        }
-
-        public void SetTextBox2(string review)
-        {
-            textBox2.Text = review;
         }
     }
 }
